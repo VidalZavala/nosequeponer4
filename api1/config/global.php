06110d31856c6b022491ce5ca81c0addbd3ce115
VidalZavala <138162096+VidalZavala@users.noreply.github.com>
2024-01-28 22:48:50 -0600

    jalo

---
+++ api1/config/global.php
@@ -0,0 +1,21 @@
+<?php 
+
+//definimos la codificacion de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE","cc");
+ 
+//datos de produccion, descomentar para pasar
+define("DB_HOST","localhost");
+
+//Nombre de la base de datos pr
+define("DB_NAME", "clase_fued");
+
+//Usuario de la base de datos pr
+define("DB_USERNAME", "root");
+
+//Contrase帽a del usuario de la base de datos
+define("DB_PASSWORD", "");
+
+?>
---
+++ api1/method.php
@@ -0,0 +1,104 @@
+<?php
+require "config/Conexion.php";
+$datos = json_decode(file_get_contents('php://input'), true);
+//print_r($_SERVER['REQUEST_METHOD']);
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        // Consulta SQL para seleccionar datos de la tabla de maestros
+        $sql = "SELECT id, nombre, apodo, tel, creado_en FROM clase_fued";
+
+        $query = $conexion->query($sql);
+
+        if ($query->num_rows > 0) {
+            $data = array();
+            while ($row = $query->fetch_assoc()) {
+                $data[] = $row;
+            }
+            // Devolver los resultados en formato JSON
+            header('Content-Type: application/json');
+            echo json_encode($data);
+        } else {
+            echo "No se encontraron registros en la tabla de clase_fued.";
+        }
+
+        $conexion->close();
+        break;
+
+    case 'POST':
+            // Recibir los datos del formulario HTML
+            $nombre = $datos['nombre'];
+            $apodo = $datos['apodo'];
+            $tel = $datos['tel'];
+
+            // Insertar los datos en la tabla de clase_fued
+            $sql = $conexion->prepare("INSERT INTO clase_fued (nombre, apodo, tel) VALUES (?,?,?)");
+            $sql->bind_param("sss", $nombre, $apodo, $tel);
+            if($sql->execute()){
+                echo "Datos insertados con exito";
+            } else {
+                echo "Error al insertar datos" . $sql->error;
+            }
+        $sql->close();
+        break;
+
+        case 'PATCH':
+        $id = $datos['id'];
+        $nombre = $datos['nombre'];
+        $apodo = $datos['apodo'];
+        $tel = $datos['tel'];
+        $foto = $datos['foto'];
+    
+        $actualizaciones = array();
+        if (!empty($apodo)) {
+            $actualizaciones[] = "apodo = '$apodo'";
+        }
+        if (!empty($tel)) {
+            $actualizaciones[] = "tel = '$tel'";
+        }
+    
+        $actualizaciones_str = implode(', ', $actualizaciones);
+        $sql = "UPDATE clase_fued SET $actualizaciones_str WHERE id = $id";
+    
+        if ($conexion->query($sql) === TRUE) {
+            echo "Registro actualizado con éxito.";
+        } else {
+            echo "Error al actualizar registro: " . $conexion->error;
+        }
+        break;
+    
+
+        case 'PUT':
+            $nombre = $datos['nombre'];
+            $id = $datos['id'];
+            $apodo = $datos['apodo'];
+            $tel = $datos['tel'];
+            $sql = "UPDATE clase_fued SET apodo = '$apodo', tel = '$tel' WHERE id = $id";
+    
+            if ($conexion->query($sql) === TRUE) {
+                echo "Registro actualizado con éxito.";
+            } else {
+                echo "Error al actualizar registro: " . $conexion->error;
+            }
+            break;
+    
+
+            case 'DELETE':
+                $id = $datos['id'];
+                
+                $stmt = $conexion->prepare("DELETE FROM clase_fued WHERE id = ?");
+                $stmt->bind_param("i", $id);
+                
+                if ($stmt->execute()) {
+                    echo "Registro eliminado con éxito.";
+                } else {
+                    echo "Error al eliminar registro: " . $stmt->error;
+                }
+                $stmt->close();
+                break;
+                    
+            default:
+                echo "Método de solicitud no válido.";
+                break;
+        }
+
+?>
